709521b38e3ec20cdb255cb1c225344912fbd507
Mabbutnem <thibault.harel1@gmail.com>
2020-06-18 14:20:47 +0200

    ForEach and Peek

---
+++ LinqExtension/LinqExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class LinqExtension
+{
+   public static IEnumerable<T> Peek<T>(this IEnumerable<T> source, Action<T> action)
+   {
+      using (var iterator = source.GetEnumerator())
+      {
+         while (iterator.MoveNext())
+         {
+            action(iterator.Current);
+         }
+      }
+
+      using (var iterator = source.GetEnumerator())
+      {
+         while (iterator.MoveNext())
+         {
+            yield return iterator.Current;
+         }
+      }
+   }
+
+   public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+   {
+      using (var iterator = source.GetEnumerator())
+      {
+         while (iterator.MoveNext())
+         {
+            action(iterator.Current);
+         }
+      }
+   }
+}
